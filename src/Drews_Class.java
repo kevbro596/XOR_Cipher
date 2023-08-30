7e63caac59f4ad8b4d139d643b347b27635bf5a2
drewdinkel <drewdinkel3417@gmail.com>
2023-08-30 10:48:28 -0500

    drew

--- src/Drews_Class.java
+++ src/Drews_Class.java
@@ -1,3 +1,12 @@
 public class Drews_Class {
+    private ArrayList<String> message;
 
+    public static void main (String args[]){
+    }
+
+    public void assignMessage(String str){
+        for(int i = 0; i < str.length(); i++){
+            message.add(str.substring(i, i+1));
+        }
+    }
 }
