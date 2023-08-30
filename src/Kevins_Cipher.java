c62fa10e41e74aac2dead00dfca5c9c82010c4c8
KevinShi <kevinbro596@gmail.com>
2023-08-30 10:35:38 -0500

    kevin

---
+++ src/Kevins_Cipher.java
@@ -0,0 +1,11 @@
+public class Kevins_Cipher {
+
+    public static void main(String args[])
+    {
+        char ch = 'a';
+        //java internally converts characters in ACSII code
+        int ASCII = ch;
+        System.out.println( ASCII);
+    }
+
+}
