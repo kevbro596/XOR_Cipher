f000a3e3ac254e0355769f515cba3bff3741e6f9
drewdinkel <drewdinkel3417@gmail.com>
2023-08-29 21:29:34 -0500

    Testing

--- src/XOR_Cipher.java
+++ src/XOR_Cipher.java
@@ -3,5 +3,6 @@ public class XOR_Cipher {
     public static void main(String args[])
     {
         System.out.println("Hello World");
+        System.out.println("Hey Kevin, its me Drew.");
     }
 }
