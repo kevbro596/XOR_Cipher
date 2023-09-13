befb8cbf1e0c9a3bf3e373775ef20cc9cd02ed91
drewdinkel <drewdinkel3417@gmail.com>
2023-09-13 15:25:10 -0500

    drew

--- src/Custom_Cipher.java
+++ src/Custom_Cipher.java
@@ -6,4 +6,50 @@ public class Custom_Cipher
     private int value = 0;
     private ArrayList<Integer> ASCII = new ArrayList<>();
 
+    public void setValues(String str, int i){
+        String[] split = str.split("");
+        for (String s : split){
+            message.add(s);
+            //Also populates ASCII to prevent future issues
+            ASCII.add(0);
+        }
+        value = i;
+    }
+
+    public void stringToAscii(){
+        for (int i = 0; i < ASCII.size(); i++){
+            char c = message.get(i).charAt(0);
+            int ascii = c;
+            ASCII.set(i, ascii);
+        }
+    }
+
+    public void asciiToString(){
+        for (int i = 0; i < ASCII.size(); i++){
+            int val = ASCII.get(i);
+            char asciiCode = (char) val;
+            String s = "" + asciiCode;
+            message.set(i, s);
+        }
+    }
+
+    public void exponent(){
+        for (int i = 0; i < ASCII.size(); i++){
+            ASCII.set(i, ((int)Math.pow(ASCII.get(i), value)));
+        }
+        System.out.println(ASCII);
+    }
+
+    public void root(){
+
+    }
+
+    public String toString(){
+        String output = "";
+        //Populates output with messages' values
+        for (String s : message){
+            output += s;
+        }
+        return output;
+    }
 }
--- src/Custom_Cipher_Runner.java
+++ src/Custom_Cipher_Runner.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Custom_Cipher_Runner {
     public static void main(String args[] )
     {
-        Custom_Cipher kevin = new Custom_Cipher();
+        Custom_Cipher test = new Custom_Cipher();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter Desired Message:: ");
         String str = scanner.nextLine();
-
         System.out.print("Enter Exponent Value:: ");
         int value = scanner.nextInt();
+        test.setValues(str, value);
+
+        test.stringToAscii();
+        test.exponent();
     }
 }
