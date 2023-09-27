502c5ae6d3bc99cbf7051305b53a67425db2cfc4
drewdinkel <drewdinkel3417@gmail.com>
2023-09-27 17:13:44 -0500

    drew

--- src/Custom_Cipher.java
+++ src/Custom_Cipher.java
@@ -5,7 +5,7 @@ public class Custom_Cipher
 {
     private String[] firstHalf = new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m"};
     private String[] secondHalf = new String[]{"z","y","x","w","v","u","t","s","r","q","p","o","n"};
-    private ArrayList<Integer> ASCII = new ArrayList<>();
+    private ArrayList<String> ASCII = new ArrayList<>();
     private ArrayList<String> message = new ArrayList<>();
 
     public void assignMessage(String str)
@@ -16,7 +16,7 @@ public class Custom_Cipher
         for (String s : split){
             message.add(s);
             //Adds 0 as placeholder
-            ASCII.add(0);
+            ASCII.add("0");
         }
     }
 
@@ -43,40 +43,39 @@ public class Custom_Cipher
         //Converting correct values to ASCII
         for(int i = 0; i < ASCII.size(); i++)
         {
-           int j = 0;
-           boolean tF = true;
-           while (j < 13){
-               //Checking if the index is in the alphabet
-               if ((message.get(i).compareToIgnoreCase(firstHalf[j]) == 0) || (message.get(i).compareToIgnoreCase(secondHalf[j]) == 0)){
-                   tF = false;
-               }
-               j++;
-           }
-           if (tF) {
-               char ch = message.get(i).charAt(0);
-               //Convert char to ASCII value (Java internal)
-               int ascii = ch;
-               ASCII.set(i, ascii);
-           }
+            int j = 0;
+            boolean tF = true;
+            while (j < 13){
+                //Checking if the index is in the alphabet
+                if ((message.get(i).compareToIgnoreCase(firstHalf[j]) == 0) || (message.get(i).compareToIgnoreCase(secondHalf[j]) == 0)){
+                    tF = false;
+                }
+                j++;
+            }
+            if (tF) {
+                char ch = message.get(i).charAt(0);
+                //Convert char to ASCII value (Java internal)
+                int ascii = ch;
+                String str = "" + ascii;
+                ASCII.set(i, str);
+            }
         }
 
         System.out.println(ASCII);
         //Reversing ASCII values
         for (int i = 0; i < ASCII.size(); i++){
-            if (ASCII.get(i) != 0){
-                int normal = ASCII.get(i);
-                int reversed = 0;
-                while (normal != 0){
-                    int digit = normal % 10;
-                    //Can't multiply by 0 first time otherwise value will stay 0
-                    if (reversed == 0){
-                        reversed = digit;
-                    }
-                    //Reversed *10 to add to next place value
-                    else{
-                        reversed = (reversed * 10) + digit;
-                    }
-                    normal /= 10;
+            if (ASCII.get(i).equals("0")) {
+                continue;
+            }
+            else{
+                String normal = ASCII.get(i);
+                String reversed = "";
+                int count = normal.length();
+                while (count > 0){
+                    String str = normal.substring(count - 1);
+                    reversed += str;
+                    normal = normal.substring(0, count - 1);
+                    count--;
                 }
                 ASCII.set(i, reversed);
             }
@@ -84,8 +83,11 @@ public class Custom_Cipher
         System.out.println(ASCII);
         //Converting to encrypted ASCII code
         for (int i = 0; i < ASCII.size(); i++){
-            if (ASCII.get(i) != 0){
-                int value = ASCII.get(i);
+            if (ASCII.get(i).equals("0")) {
+                continue;
+            }
+            else{
+                int value = Integer.valueOf(ASCII.get(i));
                 char asciiCode = (char) value;
                 String str = "" + asciiCode;
                 message.set(i, str);
